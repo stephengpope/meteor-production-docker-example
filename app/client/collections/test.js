3c22125771fb12cc585c53e2a23dc04e72dba7ec
Stephen Pope <spope@Stephens-MacBook-Pro.local>
2016-01-09 20:25:54 -0800

    updates

--- app/client/collections/test.js
+++ app/client/collections/test.js
@@ -1,2 +1,9 @@
 
 Tests = new Mongo.Collection("test");
+
+Tests.insert({
+    createdBy: Meteor.userId(),
+    createdAt: new Date(),
+    title: "My first post!",
+    content: "Today was a good day."
+});
--- app/server/bootstrap.js
+++ app/server/bootstrap.js
@@ -2,3 +2,12 @@ Meteor.startup(function () {
 
     console.log('!')
 });
+
+Tests = new Mongo.Collection("test");
+
+Tests.insert({
+    createdBy: 5,
+    createdAt: new Date(),
+    title: "My first post!",
+    content: "Today was a good day."
+});
