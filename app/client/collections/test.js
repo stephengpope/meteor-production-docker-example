c160bfb738da861ee52142d2b6147789d6133503
Stephen Pope <spope@Stephens-MacBook-Pro.local>
2016-01-26 20:33:27 -0800

    added a new test app

--- app/client/collections/test.js
+++
@@ -1,9 +0,0 @@
-
-Tests = new Mongo.Collection("test");
-
-Tests.insert({
-    createdBy: Meteor.userId(),
-    createdAt: new Date(),
-    title: "My first post!",
-    content: "Today was a good day."
-});
